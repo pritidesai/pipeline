8794ed1f8490b4fa5687ef3790cc8b0a161dd8fe
Priti Desai <pdesai@us.ibm.com>
2019-11-05 11:04:49 -0800

    taskSpec under pipelineSpec

--- pkg/apis/pipeline/v1alpha1/pipeline_validation.go
+++ pkg/apis/pipeline/v1alpha1/pipeline_validation.go
@@ -19,6 +19,7 @@ package v1alpha1
 import (
 	"context"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"strings"
 
 	"github.com/tektoncd/pipeline/pkg/list"
@@ -126,19 +127,40 @@ func (ps *PipelineSpec) Validate(ctx context.Context) *apis.FieldError {
 	// Names cannot be duplicated
 	taskNames := map[string]struct{}{}
 	for i, t := range ps.Tasks {
-		// Task names are appended to the container name, which must exist and
-		// must be a valid k8s name
-		if errSlice := validation.IsQualifiedName(t.Name); len(errSlice) != 0 {
-			return apis.ErrInvalidValue(strings.Join(errSlice, ","), fmt.Sprintf("spec.tasks[%d].name", i))
+		spew.Dump("validating task reference name or taskSpec")
+		spew.Dump(t)
+		// can't have both taskRef and taskSpec at the same time
+		if (t.TaskRef != nil && t.TaskRef.Name != "") && t.TaskSpec != nil {
+			return apis.ErrDisallowedFields("spec.taskref", "spec.taskspec")
 		}
-		// TaskRef name must be a valid k8s name
-		if errSlice := validation.IsQualifiedName(t.TaskRef.Name); len(errSlice) != 0 {
-			return apis.ErrInvalidValue(strings.Join(errSlice, ","), fmt.Sprintf("spec.tasks[%d].taskRef.name", i))
+
+		// Check that one of TaskRef and TaskSpec is present
+		if (t.TaskRef == nil || (t.TaskRef != nil && t.TaskRef.Name == "")) && t.TaskSpec == nil {
+			return apis.ErrMissingField("spec.taskref.name", "spec.taskspec")
 		}
-		if _, ok := taskNames[t.Name]; ok {
-			return apis.ErrMultipleOneOf(fmt.Sprintf("spec.tasks[%d].name", i))
+
+		// Validate TaskSpec if it's present
+		if t.TaskSpec != nil {
+			if err := t.TaskSpec.Validate(ctx); err != nil {
+				return err
+			}
 		}
-		taskNames[t.Name] = struct{}{}
+
+		if t.TaskRef != nil && t.TaskRef.Name != "" {
+			// Task names are appended to the container name, which must exist and
+			// must be a valid k8s name
+			if errSlice := validation.IsQualifiedName(t.Name); len(errSlice) != 0 {
+				return apis.ErrInvalidValue(strings.Join(errSlice, ","), fmt.Sprintf("spec.tasks[%d].name", i))
+			}
+			// TaskRef name must be a valid k8s name
+			if errSlice := validation.IsQualifiedName(t.TaskRef.Name); len(errSlice) != 0 {
+				return apis.ErrInvalidValue(strings.Join(errSlice, ","), fmt.Sprintf("spec.tasks[%d].taskRef.name", i))
+			}
+			if _, ok := taskNames[t.Name]; ok {
+				return apis.ErrMultipleOneOf(fmt.Sprintf("spec.tasks[%d].name", i))
+			}
+			taskNames[t.Name] = struct{}{}
+		}
 	}
 
 	// All declared resources should be used, and the Pipeline shouldn't try to use any resources
